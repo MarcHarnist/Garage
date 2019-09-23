052d346d2ac306b86e62ab889a7990479354811a
GCKEVL <GCKEVL@192.168.1.88>
2019-09-23 14:32:54 +0200

    add tests JUnit

---
+++ Garage/src/fr/marcharnist/tests/GarageTest.java
@@ -0,0 +1,14 @@
+package fr.marcharnist.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GarageTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
